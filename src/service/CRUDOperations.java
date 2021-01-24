0bf53daf99d1b56e2413eb700ab4eb6c76723392
unknown <srimathi20062000@yahoo.com>
2021-01-24 12:36:48 +0530

    CRUD

--- src/service/CRUDOperations.java
+++ src/service/CRUDOperations.java
@@ -2,6 +2,8 @@ package service;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 import model.Blog;
@@ -11,19 +13,22 @@ public class CRUDOperations {
 	
 	public List<Blog> createBlog(Blog blog) throws IOException {
 		list.add(blog);
+		Collections.sort(list,(Blog blog1,Blog blog2)->(blog1.getBlogTitle().toUpperCase()).compareTo(blog2.getBlogTitle().toUpperCase()));
+		displayList();
 		return list;
 	}
 	
+	private void displayList() {
+		int length=list.size();
+		for(int i=0;i<length;i++)
+		{
+			System.out.println(list.get(i).getBlogTitle()+" "+list.get(i).getBlogDescription());
+		}
+		
+	}
+
 	public List<Blog> viewBlog(Blog blog) throws IOException{
-		return null;
-		
-	}
-	public List<Blog> updateBlog(Blog blog) throws IOException{
-		return null;
-	
-	}
-	public List<Blog> deleteBlog(Blog blog) throws IOException{
-		return null;
-		
-	}
+		return list;
+		}
+
 }
